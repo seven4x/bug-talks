2ab2dcfc6fbe27581567ee4b736384bb225e97e0
seven4x <leigoto@gmail.com>
2020-10-15 22:35:30 +0800

    once java版

---
+++ issue1/src/main/java/io/seven4x/lib/sync/Once.java
@@ -0,0 +1,59 @@
+package io.seven4x.lib.sync;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Once {
+
+    private final AtomicBoolean done = new AtomicBoolean(false);
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public void Do(Silent func) {
+        if (!done.get()) {
+            doSlow(func);
+        }
+    }
+
+    private void doSlow(Silent func) {
+        try {
+            lock.lock();
+            if (!done.get()) {
+                func.apply();
+                done.set(true);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+}
+
+@FunctionalInterface
+interface Silent {
+    void apply();
+}
+
+class Example {
+
+    Once once = new Once();
+
+    public void test(String s) {
+        once.Do(() -> {
+            System.out.println("once...只执行一次");
+        });
+        //业务方法
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        Example example = new Example();
+        for (int i = 0; i < 10; i++) {
+            var t = new Thread(() -> {
+                example.test(Thread.currentThread().getName());
+            });
+            t.setName("t_" + i);
+            t.start();
+        }
+    }
+}
